386b32d3e6a09f45c817658eef47e2e13aee18c3
Elay Sason <elaysason@gmail.com>
2026-02-12 18:51:37 +0200

    added parse func to base scarper

--- scrapers/base.py
+++ scrapers/base.py
@@ -1,5 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import List
+from enum import Enum
+from typing import Any, Dict, Generator, Optional
+
+
+class FileType(Enum):
+    PRICE_FULL = "PriceFull"
+    PRICE_DELETA = "Price"
+    PROMO = "PromoFull"
+    PROMO_DELETA = "Promo"
 
 
 class BaseScraper(ABC):
@@ -10,6 +18,19 @@ class BaseScraper(ABC):
     def chain_name(self) -> str:
         pass
 
+    @property
     @abstractmethod
     def online_store(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def chain_code(self) -> str:
+        pass
+
+    def get_latest_file_url(selfself, file_type: FileType) -> Optional[str]:
+        pass
+
+    @abstractmethod
+    def parse(self, file_path: str) -> Generator[Dict[str, Any], None, None]:
         pass
